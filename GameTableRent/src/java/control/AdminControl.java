cc8c05c00b2f4ddf78b02c1c78f3f49390b8e174
matti124 <mattiafattoruso@gmail.com>
2024-06-19 18:02:31 +0200

    Creazione Servlet Per azioni di Admin e Implementazione Filtro per Admin

---
+++ GameTableRent/src/java/control/AdminControl.java
@@ -0,0 +1,89 @@
+package control;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.OrdineDAO;
+import model.OrdineDTO;
+
+/*
+ * AZIONI GESTITE DALLA SERVLET ADMIN CONTROL:
+ * 1) VEDERE TUTTI GLI ORDINI EFFETTUATI IN UN CERTO LASSO DI TEMPO
+ * 2) ELIMINARE UN PRODOTTO 
+ * 3) MODIFICARE UN PRODOTTO
+ * 4) AGGIUNGERE UN PRODOTTO
+ * 
+ * 
+ * 
+ */
+@WebServlet("/AdminControl")
+public class AdminControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String azione=request.getParameter("action");
+		
+		switch (azione) {
+		
+		
+		case "allOrdersByDate": 
+			this.AllOrders(request, response);
+			break;
+			
+		case "addProduct": //mi indirizza alla pagina in cui compilerò il form per il nuovo elemento la quale passerà i dati a ProductControl?action=AddProduct
+			response.sendRedirect(request.getContextPath()+"/NewProduct.jsp");
+			break;
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String azione=request.getParameter("action"); 
+		
+		switch (azione) {
+		
+		
+		case "updateProd":
+			response.sendRedirect(request.getContextPath()+"/UpdateProduct.jsp"); //mi indirizza alla pagina form dove potrò aggiornare il prodotto e i dati modificati verranno inoltrati a ProuctControl?action=updateProduct
+		
+		case "removeProd":
+			request.getRequestDispatcher("/ProductoControl?action=eliminaProdotto");
+		}
+	}
+	
+	
+
+	private void AllOrders(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Timestamp start=Timestamp.valueOf(request.getParameter("Start")); //sarà necessaria una regular expression di javascriprt per essere sicuro che sia corretta la data
+		Timestamp end=Timestamp.valueOf(request.getParameter("End"));
+		OrdineDAO dao=new OrdineDAO();
+		ArrayList<OrdineDTO> ordini=dao.doRetrieveByDate(start, end);
+		request.setAttribute("listaOrdini", ordini);
+		request.getRequestDispatcher("/OthersOrders.jsp").forward(request,response); //pagina per vedere tutti gli ordini in uno specifico arco di tempo
+	}
+
+
+}
