3d1c83358569ce896216649040c5195202ef53cb
matti124 <mattiafattoruso@gmail.com>
2024-06-19 18:02:31 +0200

    Creazione Servlet Per azioni di Admin e Implementazione Filtro per Admin

---
+++ GameTableRent/src/java/control/AdminFilter.java
@@ -0,0 +1,62 @@
+package control;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.UtenteDTO;
+
+/**
+ * Servlet Filter implementation class FilterForToken
+ */
+@WebFilter(filterName = "AdminFilter", urlPatterns = {"/AdminControl", "/ProductControl?action=update", "/ProductoControl?action=elimina", "/ProductControl?action=aggiungi"})
+public class AdminFilter implements Filter {
+
+    /**
+     * Default constructor.
+     */
+    public AdminFilter() {
+    }
+
+    /**
+     * @see Filter#destroy()
+     */
+    public void destroy() {
+        // Cleanup code if needed
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        HttpSession session = request.getSession(false);
+        
+        boolean isAdmin = session != null && session.getAttribute("utente") != null && ((UtenteDTO) session.getAttribute("utente")).getIsAdmin() == 1;
+
+        // Verifica se l'utente è admin per i servlet specificati
+        if (isAdmin) {
+            filterChain.doFilter(request, response); // Prosegui con la richiesta
+        } else {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Accesso non consentito");
+        }
+    }
+
+    /**
+     * @see Filter#init(FilterConfig)
+     */
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+}
